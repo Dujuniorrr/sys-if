af89272e7f4e29221055aea7093f5de4d556b625
Dujuniorrr <105893394+Dujuniorrr@users.noreply.github.com>
2022-12-23 17:45:55 -0300

    Add files via upload

---
+++ sysif/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('aluno/', include('aluno.urls')),
+    path('turma/', include('turma.urls')),
+    path('professor/', include('professor.urls')),
+    path('disciplina/', include('disciplina.urls')),
+    path('curso/', include('curso.urls')),
+    path('', include('usuarios.urls')),
+]
